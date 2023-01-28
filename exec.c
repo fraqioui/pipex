c3bfae48c6694283dcdfc32c4b49b6575471ba51
Fatima Ezzahra Raqioui <116555960+fraqioui@users.noreply.github.com>
2023-01-28 17:01:06 +0100

    -

--- exec.c
+++ exec.c
@@ -13,16 +13,22 @@
 
 int	ft_child_a(t_pp pp, char **path_s, char **env)
 {
+	//commands read from standard input and display their output to the standard output
+	//So that, I should duplicate the stdin by the fd of the input file and the stdout by the write end of the pipe
 	if (dup2(pp.end[1], 1) == -1 || dup2(pp.fd_in, 0) == -1)
 		ft_exit("failed to duplicate the file descriptor");
+	//closing the input file and the write end of the pipe after dup2 function.
 	ft_close_ends(pp.end[1], pp.fd_in, -1, -1);
+	//finding command path
 	cmd_path(&pp, pp.cmd_a[0], path_s);
+	//freeing
 	freeing(pp.cmd_b, path_s, NULL, NULL);
+	//executing the cmd via execve function, if the execve succeeds if will return nothing and terminate the process, else it will return -1.
 	execve(pp.cmd_path, pp.cmd_a, env);
 	freeing(pp.cmd_a, NULL, NULL, pp.cmd_path);
 	return (-1);
 }
-
+//the same process as the child_a function
 int	ft_child_b(t_pp pp, char **path_s, char **env)
 {
 	if (dup2(pp.end[0], 0) == -1 || dup2(pp.fd_out, 1) == -1)
